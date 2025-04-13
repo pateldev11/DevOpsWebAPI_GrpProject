38b2c08319b353380d6186460a274e623d3d1b23
Devkumar Patel <dpat1072@my.centennialcollege.ca>
2025-04-13 17:32:25 -0400

    CHNAGES MADE

--- DevOpsWebAPI/Program.cs
+++ DevOpsWebAPI/Program.cs
@@ -1,20 +1,4 @@
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+app.MapGet("/", () => "Hello World!");
 app.Run();
